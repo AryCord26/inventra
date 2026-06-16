1b4963d955e9b3e989ef3694ebbbfa3dea0949d1
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 09:58:49 -0300

    Create userService.js

---
+++ backend/src/services/userService.js
@@ -0,0 +1,46 @@
+const prisma = require('../config/database');
+
+class UserService {
+
+  async getAll() {
+
+    return await prisma.user.findMany();
+  }
+
+  async getById(id) {
+
+    return await prisma.user.findUnique({
+      where: {
+        id: Number(id)
+      }
+    });
+  }
+
+  async create(data) {
+
+    return await prisma.user.create({
+      data
+    });
+  }
+
+  async update(id, data) {
+
+    return await prisma.user.update({
+      where: {
+        id: Number(id)
+      },
+      data
+    });
+  }
+
+  async delete(id) {
+
+    return await prisma.user.delete({
+      where: {
+        id: Number(id)
+      }
+    });
+  }
+}
+
+module.exports = new UserService();
