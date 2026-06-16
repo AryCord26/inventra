9c0e448657d97ec3a879fe5255b7c3ccbcba7e10
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 10:40:29 -0300

    Create next.config.js

---
+++ web/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+
+  experimental: {
+    optimizePackageImports: [
+      "axios"
+    ]
+  }
+};
+
+module.exports = nextConfig;
