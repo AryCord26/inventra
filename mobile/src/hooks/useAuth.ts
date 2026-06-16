e098539bbdd64e7ead849ba3368932aaeea47c8f
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 11:07:26 -0300

    Update useAuth.ts

--- mobile/src/hooks/useAuth.ts
+++ mobile/src/hooks/useAuth.ts
@@ -1,10 +1,6 @@
-import { useContext }
-from 'react';
+import { useContext } from 'react';
 
-import {
-  AuthContext
-}
-from '../context/AuthContext';
+import { AuthContext } from '../context/AuthContext';
 
 export function useAuth() {
   return useContext(AuthContext);
