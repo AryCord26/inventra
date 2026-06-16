7ac50f56a6a40d2e10e16d13316e729581b4c5e9
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 10:32:51 -0300

    Create DashboardCard.tsx

---
+++ web/src/components/DashboardCard.tsx
@@ -0,0 +1,42 @@
+interface Props {
+  title: string;
+  value: number;
+}
+
+export default function DashboardCard({
+  title,
+  value
+}: Props) {
+
+  return (
+    <div
+      className="
+        bg-white
+        rounded-xl
+        shadow
+        p-6
+      "
+    >
+
+      <h3
+        className="
+          text-gray-500
+          text-sm
+        "
+      >
+        {title}
+      </h3>
+
+      <p
+        className="
+          text-3xl
+          font-bold
+          mt-2
+        "
+      >
+        {value}
+      </p>
+
+    </div>
+  );
+}
