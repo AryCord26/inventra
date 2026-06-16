f0e5dcd0df7f381e6c358aebd11ebec7091bb3fd
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 10:44:46 -0300

    Create AuthContext.tsx

---
+++ mobile/src/context/AuthContext.tsx
@@ -0,0 +1,61 @@
+import {
+  createContext,
+  useState
+} from 'react';
+
+import AsyncStorage
+from '@react-native-async-storage/async-storage';
+
+import { authService }
+from '../services/authService';
+
+export const AuthContext =
+createContext({} as any);
+
+export function AuthProvider({
+  children
+}: any) {
+
+  const [user, setUser] =
+  useState(null);
+
+  async function login(
+    email: string,
+    password: string
+  ) {
+
+    const response =
+    await authService.login(
+      email,
+      password
+    );
+
+    await AsyncStorage.setItem(
+      'token',
+      response.token
+    );
+
+    setUser(response.user);
+  }
+
+  async function logout() {
+
+    await AsyncStorage.removeItem(
+      'token'
+    );
+
+    setUser(null);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        login,
+        logout
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
