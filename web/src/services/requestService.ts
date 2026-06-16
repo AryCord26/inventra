37d2238796b94e4e406ff05373ada71f1530c8ae
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 10:15:13 -0300

    Create requestService.ts

---
+++ web/src/services/requestService.ts
@@ -0,0 +1,20 @@
+import api from './api';
+
+export const requestService = {
+
+  async getAll() {
+
+    const response =
+      await api.get('/requests');
+
+    return response.data;
+  },
+
+  async create(data: any) {
+
+    const response =
+      await api.post('/requests', data);
+
+    return response.data;
+  },
+};
