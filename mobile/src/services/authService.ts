01385ece82133f5d7c3fe96f73a3d8f630c2b06a
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 10:44:02 -0300

    Create authService.ts

---
+++ mobile/src/services/authService.ts
@@ -0,0 +1,19 @@
+import api from './api';
+
+export const authService = {
+
+  async login(
+    email: string,
+    password: string
+  ) {
+
+    const response =
+      await api.post('/auth/login', {
+        email,
+        password
+      });
+
+    return response.data;
+  }
+
+};
