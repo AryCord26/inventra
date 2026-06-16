f3901d15efb7dba5f7e4cba8873cb20156c91029
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 10:33:53 -0300

    Update Dashboard.ts

--- web/src/types/Dashboard.ts
+++ web/src/types/Dashboard.ts
@@ -1,7 +1,16 @@
+export interface LowStockProduct {
+  id: number;
+  name: string;
+  quantity: number;
+  minimumStock: number;
+}
+
 export interface Dashboard {
   totalUsers: number;
   totalProducts: number;
   totalRequests: number;
   pendingRequests: number;
   approvedRequests: number;
+
+  lowStockProducts: LowStockProduct[];
 }
