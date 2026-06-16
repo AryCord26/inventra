b5354aca034fcfb875a597fb324bf9a573718c87
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 10:37:54 -0300

    Update requestService.ts

--- web/src/services/requestService.ts
+++ web/src/services/requestService.ts
@@ -13,8 +13,35 @@ export const requestService = {
   async create(data: any) {
 
     const response =
-      await api.post('/requests', data);
+      await api.post(
+        '/requests',
+        data
+      );
 
     return response.data;
   },
+
+  async update(
+    id: number,
+    data: any
+  ) {
+
+    const response =
+      await api.put(
+        `/requests/${id}`,
+        data
+      );
+
+    return response.data;
+  },
+
+  async delete(id: number) {
+
+    const response =
+      await api.delete(
+        `/requests/${id}`
+      );
+
+    return response.data;
+  }
 };
