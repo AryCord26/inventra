73b3e878074a9edb9fb46c70988b12d04cca9974
Ary Cordeiro <ary_cordeiro@outlook.com>
2026-06-16 10:45:17 -0300

    Create AppNavigator.tsx

---
+++ mobile/src/navigation/AppNavigator.tsx
@@ -0,0 +1,59 @@
+import {
+ NavigationContainer
+}
+from '@react-navigation/native';
+
+import {
+ createNativeStackNavigator
+}
+from '@react-navigation/native-stack';
+
+import LoginScreen
+from '../screens/LoginScreen';
+
+import DashboardScreen
+from '../screens/DashboardScreen';
+
+import ProductsScreen
+from '../screens/ProductsScreen';
+
+import RequestsScreen
+from '../screens/RequestsScreen';
+
+const Stack =
+createNativeStackNavigator();
+
+export default function AppNavigator() {
+
+ return (
+
+  <NavigationContainer>
+
+   <Stack.Navigator>
+
+    <Stack.Screen
+      name="Login"
+      component={LoginScreen}
+    />
+
+    <Stack.Screen
+      name="Dashboard"
+      component={DashboardScreen}
+    />
+
+    <Stack.Screen
+      name="Produtos"
+      component={ProductsScreen}
+    />
+
+    <Stack.Screen
+      name="Solicitacoes"
+      component={RequestsScreen}
+    />
+
+   </Stack.Navigator>
+
+  </NavigationContainer>
+
+ );
+}
